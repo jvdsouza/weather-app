da3caa9d19a5bb113d5608e7548191d42341ab9e
jvdsouza <jvincentdsouza@gmail.com>
2019-01-19 13:23:20 +1100

    initial commit and added basic functionality for API calls to show weather for London in celcius and farenheit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from './components/Card/Card';
 import './App.css';
 
+const appID="c3860854e4b445d4a7d20312191901";
+//API key obtained from https://www.apixu.com/
+//free api key
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Location: "London",
+      WeatherDataC: "",
+      WeatherDataF: "",
+      input: ""
+    }
+  }
+
+  componentDidMount() { //fetch the weather api
+    fetch(`https://api.apixu.com/v1/current.json?key=${appID}&q=${this.state.Location}`)
+      .then(response => {
+        //return response.json();
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          WeatherDataC: data.current.temp_c,
+          WeatherDataF: data.current.temp_f
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Card
+        weatherCelc={this.state.WeatherDataC}
+        weatherFar={this.state.WeatherDataF}
+        location={this.state.Location}
+      />
     );
   }
 }
---
+++ src/containers/Weather/Weather.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Weather extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Location: ''
+    }
+  }
+
+  handleInput
+}
